ff9cf2012941c269fbc00ad0d76a4d409b795c59
Felipe-Euzebio <felipe.euzebio.ti@gmail.com>
2025-03-31 23:16:02 -0300

    Adicionar DTOs de requisição (TarefaRequest) e resposta (TarefaResponse) para a entidade Tarefa

---
+++ API/Dtos/TarefaRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Dtos;
+
+public class TarefaRequest
+{
+    public string? Titulo { get; set; }
+
+    public string? Descricao { get; set; }
+
+    public DateTime PrazoEntrega { get; set; }
+}
---
+++ API/Dtos/TarefaResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Dtos;
+
+public class TarefaResponse
+{
+    public int Id { get; set; }
+
+    public string? Titulo { get; set; }
+
+    public string? Descricao { get; set; }
+
+    public DateTime PrazoEntrega { get; set; }
+}
