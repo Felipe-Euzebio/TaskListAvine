91a081138eba2f9276c32fb556321ece85718d47
Felipe-Euzebio <felipe.euzebio.ti@gmail.com>
2025-03-31 22:38:37 -0300

    Adicionar classe para a entidade Tarefa

---
+++ API/Models/Tarefa.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Models;
+
+public class Tarefa
+{
+    public int Id { get; set; }
+
+    public string? Titulo { get; set; }
+
+    public string? Descricao { get; set; }
+
+    public DateTime PrazoEntrega { get; set; }
+}
