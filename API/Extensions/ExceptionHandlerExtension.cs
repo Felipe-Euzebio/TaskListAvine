c83341bb2227e65a3dee13a2547eae3b6dd1351c
Felipe-Euzebio <felipe.euzebio.ti@gmail.com>
2025-04-02 10:22:47 -0300

    Adicionar método de extensão de IApplicationBuilder para adicionar middleware de exceção global

---
+++ API/Extensions/ExceptionHandlerExtension.cs
@@ -0,0 +1,46 @@
+﻿using API.Dtos;
+using Microsoft.AspNetCore.Diagnostics;
+using System.Net;
+using System.Text.Json;
+
+namespace API.Extensions;
+
+public static class ExceptionHandlerExtension
+{
+    public static void ConfigureExceptionHandler(this IApplicationBuilder app)
+    {
+        app.UseExceptionHandler(appError =>
+        {
+            appError.Run(async context =>
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.ContentType = "application/json";
+
+                var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+
+                if (contextFeature != null)
+                {
+                    var env = context.RequestServices.GetRequiredService<IWebHostEnvironment>();
+
+                    bool isDevelopment = env.IsDevelopment();
+
+                    var response = new ErrorResponse
+                    {
+                        StatusCode = (HttpStatusCode)context.Response.StatusCode,
+                        Message = contextFeature.Error.Message,
+                        StackTrace = isDevelopment ? contextFeature.Error.StackTrace?.ToString() : null
+                    };
+
+                    var jsonOptions = new JsonSerializerOptions
+                    {
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                    };
+
+                    var json = JsonSerializer.Serialize(response, jsonOptions);
+
+                    await context.Response.WriteAsync(json);
+                }
+            });
+        });
+    }
+}
